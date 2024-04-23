e29ea8340d51d8eae0ab9dbca75576e27990ed7c
Aryan Salaria <arayn.32@gmail.com>
2024-04-23 23:41:49 +0530

    The Bot can now Ping

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -17,9 +17,7 @@ module.exports = (client) => {
         const command = require(`../../commands/${folder}/${file}`);
         client.commands.set(command.data.name, command);
         commandArray.push(command.data.toJSON());
-        console.log(
-          `Command: ${command.data.name} has been passed through the handler`
-        );
+        //console.log(`Command: ${command.data.name} has been passed through the handler`);
       }
     }
 
