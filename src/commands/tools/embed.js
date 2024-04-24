a818878c4a43b001679e0233d4c15d84fc6476cd
Aryan Salaria <arayn.32@gmail.com>
2024-04-24 21:17:55 +0530

    Added Embeds

--- src/commands/tools/embed.js
+++ src/commands/tools/embed.js
@@ -1,4 +1,4 @@
-const {SlashCommandBuilder } = require('discord.js');
+const {SlashCommandBuilder, EmbedBuilder, Embed} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,7 +6,41 @@ module.exports = {
         .setDescription('Returns an embed.'),
     async execute(interaction, client)
     {
-        
-        
-    }
+        const embed =  new EmbedBuilder()
+        .setTitle(`Hello! My name is Anomaly`)
+        .setDescription('How are you?')
+        .setColor(0x18e1ee)
+        .setImage(client.user.displayAvatarURL())
+        .setThumbnail(client.user.displayAvatarURL())
+        .setTimestamp(Date.now())
+        .setAuthor({
+            url: `https://github.com/Aryan-747`,
+            iconURL: interaction.user.displayAvatarURL(),
+            name: interaction.user.tag
+        })
+        .setFooter({
+            iconURL: client.user.displayAvatarURL(),
+            text: client.user.tag
+        })
+
+        .setURL(`https://github.com/Aryan-747`)
+
+        .addFields([
+            {
+                name: 'Field 1',
+                value: 'Field value 1',
+                inline: true
+            },
+            {
+                name: 'Field 2',
+                value: 'Field value 2',
+                inline: true
+            }
+
+          ]);
+
+        await interaction.reply({
+            embeds: [embed]
+        });
+    },
 };
